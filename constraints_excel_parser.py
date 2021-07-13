4a21f6a080d89dab2bfce5cd45557837523cac48
patdan10 <47488377+patdan10@users.noreply.github.com>
2021-07-13 15:56:32 -0400

    Delete constraints_excel_parser.py

--- constraints_excel_parser.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the excel
-def get_excel(fp, fp2, fp3):
-    maxes = pd.read_excel(fp2, sheet_name=None)
-    mins = pd.read_excel(fp3, sheet_name=None)
-    return pd.read_excel(fp, sheet_name=None), maxes, mins
