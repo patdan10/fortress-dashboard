15b83a75257394af7613428615266e00dfca3a53
patdan10 <47488377+patdan10@users.noreply.github.com>
2021-07-19 10:20:50 -0400

    Update dashboard_graph_creator.py

--- dashboard_graph_creator.py
+++ dashboard_graph_creator.py
@@ -1,12 +1,17 @@
 from matplotlib import pyplot as plt
 # Return the scatterplot with the correct axes and filters
 def scatter_matplot_returner(dataX, dataY, nodeOptionX, nodeOptionY, dataOptionX, dataOptionY):
+    dataX = dataX.map(lambda x: float(x))
+    dataY = dataY.map(lambda x: float(x))
+    
     # Generate unmarked chart
     fig, ax = plt.subplots()
     ax.axhline(y=0, lw=1.5, color='k')
     ax.axvline(x=0, lw=1.5, color='k')
 
-    go = plt.scatter(y=dataY, x=dataX)
+    plt.plot(dataX, dataY, 'o')
+    m = np.polyfit(dataX, dataY, 1)
+    plt.plot(dataX, m[0] * dataX + m[1])
 
     # Generate and return the plot
     ax.set_title(str(dataOptionX) + " for " + str(nodeOptionX) + " vs " + str(dataOptionY) + " for " + str(nodeOptionY))
