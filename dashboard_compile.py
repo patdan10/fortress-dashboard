45372550e1cd4984e7f97c8e3ff455939c1c7152
patdan10 <47488377+patdan10@users.noreply.github.com>
2021-07-24 16:15:41 -0400

    Update dashboard_compile.py

--- dashboard_compile.py
+++ dashboard_compile.py
@@ -2,10 +2,10 @@ import pandas as pd, streamlit as st
 import congestion_database_pull, nodes_database_pull, dashboard_graph_creator, weather_temperature_pull, filter_finder
 from streamlit import caching
 
-# 'Load', 'Station Temperature', 'Station Wind', 'Region 1 Wind', 'Region 2 Wind', 'Region 3 Wind', 'Region 4 Wind', 'Region 5 Wind', 'Sum of All Wind', 
-# 'Sum of All Wind', 
 
+# Dashboard compilation
 def compile():
+    # The node options, along with helper lists below.
     nodeOptions = ['Load', 'Station Temperature', 'Station Wind', 'Region 1 Wind', 'Region 2 Wind', 'Region 3 Wind', 'Region 4 Wind', 'Region 5 Wind', 'Sum of All Wind', 'DA-RT', 'RT-DA', 'Spread', 'Net Demand']
     nodeExclusive = ['DA-RT', 'RT-DA', 'DALMP']
     components = {'DA-RT': ['DALMP', 'RTLMP'], 'RT-DA': ['RTLMP', 'DALMP'], 'Spread': ['DALMP', 'RTLMP', 'RTLMP', 'DALMP'], 'DALMP': ['DALMP']}
@@ -14,73 +14,97 @@ def compile():
     # If password, enter the dataframe
     password = st.text_input("Password: ")
     if password == "constraint123" or True:
+        
+        # Get the constraints, nodes, and iems, which are all cached
         cons = congestion_database_pull.get_constraints()
         nodes = nodes_database_pull.get_node_names()
         iems = weather_temperature_pull.get_iems().sort_values(by='IEMs')
-
+        
+        # Titles
         st.title("Constraints Data Visualizer")
-
         st.header("Constraint Selector")
 
-        # Choose based on regex
+        # Choose constraint
         conSelect = st.selectbox(
             "Which Constraint?",
             cons['Cons_name']
         )
         
+        # Get the constraint information, and the mins and maxes of it
         row = cons.loc[cons['Cons_name'] == conSelect]
         minimaxes = congestion_database_pull.get_minimaxes(row['PriceDate'].iloc[0], (row['Hour'].iloc[0].item()))
-
+        
+        # Write the mins and maxes
         st.write(minimaxes[1])
         st.write(minimaxes[0])
-
+        
+        # Set up the nodes list with the mins and maxes at the top in order
         pt1 = minimaxes[1]['Node'].values.tolist()
         pt2 = minimaxes[0]['Node'].values.tolist()
         allNodes = nodes['NodeName'].copy()
         allNodes.sort_values(inplace=True)
         allNodes = list(allNodes)
-
+        
+        # Remove them
         for i in range(len(pt1)):
             allNodes.remove(pt1[i])
             allNodes.remove(pt2[i])
-
+        
+        # Add them at the top
         for i in range(len(pt1))[::-1]:
             allNodes = [pt1[i]] + allNodes
             allNodes = [pt2[i]] + allNodes
 
 
-
+        # Write the X selector
         st.header("X Data Selector")
+        
+        # Pick the information
         nodeSelectX, dataX, dataSelectX = info_picker(nodeOptions, iems, nodeExclusive, allNodes, components, nodes, "X")
-
+        
+        # Filter the data if selected
         st.subheader("X Data Filter")
         doFilter = st.checkbox("Do you want to filter X?")
+        
+        # If filtered
         if doFilter:
+            # Filter the info
             tempy = filter(dataX, nodeOptions+['DALMP'], "X", iems, nodeExclusive, allNodes, components, nodes)
+            
+            # If it works, do it. If not, write no filter
             if len(tempy) == 0:
                 st.write("No Filter")
             else:
                 dataX = tempy
 
 
-
+        # Same selection for Y
         st.header("Y Data Selector")
+        
+        # Pick info for Y
         nodeSelectY, dataY, dataSelectY = info_picker(nodeOptions, iems, nodeExclusive, allNodes, components, nodes, "Y")
-
+        
+        # Filter option
         st.subheader("Y Data Filter")
         doFilter = st.checkbox("Do you want to filter Y?")
+        
+        # If filtered
         if doFilter:
+            # Filter the info
             tempy = filter(dataY, nodeOptions+['DALMP'], "Y", iems, nodeExclusive, allNodes, components, nodes)
+            
+            # If it works, do it. If not, write no filter
             if len(tempy) == 0:
                 st.write("No Filter")
             else:
                 dataY = tempy
 
 
-
+        # Merge the information into one frame
         frame = pd.merge(dataX, dataY, how='left', on=['PriceDate', 'Hour'])
         frame.dropna(axis=0, how='any', inplace=True)
-
+        
+        # If they are the same, add the endings
         if dataSelectX == dataSelectY:
             dataSelectX += '_x'
             dataSelectY += '_y'
@@ -89,147 +113,196 @@ def compile():
         doColor = st.checkbox("Do you want to color the points?")
         doColor = True
         if doColor:
+            # Pick the color for each
             colorData = color_picker(allNodes, nodes, components, colors)
             frame = pd.merge(frame, colorData, how='left', on=['PriceDate', 'Hour'])
             frame.dropna(axis=0, how='any', inplace=True)
         else:
+            # Uncomment for random gradiant
             """gradiant = []
             step = 100.0/len(frame[dataSelectX])
             for i in range(len(frame[dataSelectX])):
                 gradiant.append(step*i)"""
+            
+            # Makee them all the same
             frame['Color'] = 0
 
-
+        # Make the scatterplot and plot it
         plot = dashboard_graph_creator.scatter_matplot_returner(frame[dataSelectX], frame[dataSelectY], nodeSelectX, nodeSelectY, dataSelectX, dataSelectY, frame['Color'], colors, doColor)
         st.pyplot(plot)
-
+        
+        # Convert to floats
         frame[dataSelectX] = frame[dataSelectX].map(lambda x: float(x))
         frame[dataSelectY] = frame[dataSelectY].map(lambda x: float(x))
-
+        
+        # Get the correlation and write it
         pearson = frame[dataSelectX].corr(frame[dataSelectY])
         st.write("Correlation between Data: " + str(pearson))
         st.write(frame)
 
 
 
-
+# Get the colors for the nodes
 def color_picker(allNodes, nodes, components, colors):
     dataSelect = st.selectbox(
         "Which datapoint to color by?",
         ("DA-RT", "RT-DA", "Spread")
     )
-
+    
+    # Depending on the selected point, get different information. If spread, do this
     if dataSelect == 'Spread':
+        # Choose long node
         nodeSelectShort = st.selectbox(
             "Which Node Long?",
             allNodes
         )
         data = pd.DataFrame()
+        
+        # Get DA-RT for Long
         for bit in components['DA-RT']:
+            # Get data for Long node
             temp = nodes_database_pull.get_node_info(nodes.loc[nodes['NodeName'] == nodeSelectShort].iloc[0]['Node_ID'], bit, dataSelect)
+            # If gotten correctly, put it in the dataframe
             if data.empty:
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 data = temp
             else:
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 data["Color"] -= temp["Color"]
-
+        
+        
+        # Choose short node
         nodeSelectLong = st.selectbox(
             "Which Node Short?",
             allNodes
         )
         dataTemp = pd.DataFrame()
+        
+        # Get RT-DA for Short
         for bit in components['RT-DA']:
+            # Get data for Short node
             temp = nodes_database_pull.get_node_info(nodes.loc[nodes['NodeName'] == nodeSelectLong].iloc[0]['Node_ID'], bit, dataSelect)
+            # If gotten correctly, put it in dataframe
             if dataTemp.empty:
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 dataTemp = temp
             else:
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 dataTemp["Color"] -= temp["Color"]
+        # Add long and short together for spread
         data["Color"] += dataTemp["Color"]
     else:
+        # If DA-RT or RT-DA
+        # Select Node
         nodeSelect = st.selectbox(
             "Which Node?",
             allNodes
         )
         data = pd.DataFrame()
+        
+        # For componant in data selected
         for bit in components[dataSelect]:
+            # Get the bit of information
             temp = nodes_database_pull.get_node_info(nodes.loc[nodes['NodeName'] == nodeSelect].iloc[0]['Node_ID'],bit, dataSelect)
             if data.empty:
+                # If the first one, then set it first
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 data = temp
             else:
+                # If the second one, then subtract it
                 temp = temp.rename(columns={dataSelect: 'Color'})
                 data["Color"] -= temp["Color"]
+    
+    # Set color for information
     data['Color'] = data['Color'].map(lambda x: colors[0] if x < -2 else (colors[1] if x < 1 else (colors[2] if x < 5 else colors[3])))
     return data
 
 
 
 
-
+# Information getter
 def info_picker(nodeOptions, iems, nodeExclusive, allNodes, components, nodes, point):
-    # Choose based on regex
+    # Choose piece of information
     dataSelect = st.selectbox(
         "Which Info On " + point + "?",
         nodeOptions
     )
-
+    
+    # If teemperature
     if dataSelect == 'Station Temperature':
         nodeSelect = st.selectbox(
             "Which IEM for Temperature for " + point + "?",
             iems
         )
+        # Select temperature at chosen point
         data = weather_temperature_pull.get_temperature(nodeSelect)
     elif dataSelect == 'Station Wind':
+        # If wind temperatuer
         nodeSelect = st.selectbox(
             "Which IEM for Wind for " + point + "?",
             iems
         )
+        # Select wind at certain point
         data = weather_temperature_pull.get_wind(nodeSelect)
     elif dataSelect in nodeExclusive:
+        # If it needs a node
         nodeSelect = st.selectbox(
             "Which Node On " + point + "?",
             allNodes
         )
         data = pd.DataFrame()
+        # Get the relevant bit information at each node
         for bit in components[dataSelect]:
+            # Get the information
             temp = nodes_database_pull.get_node_info(nodes.loc[nodes['NodeName'] == nodeSelect].iloc[0]['Node_ID'],
                                                      bit, dataSelect)
+            # Select information and subtract it
             if data.empty:
                 data = temp
             else:
                 data[dataSelect] -= temp[dataSelect]
     elif dataSelect == 'Spread':
+        # If it's spread, get long and short
         nodeSelectShort = st.selectbox(
             "Which Node Long On " + point + "?",
             allNodes
         )
         data = pd.DataFrame()
+        
+        # Get the DA-RT information
         for bit in components['DA-RT']:
+            # Get bit of information, and select it
             temp = nodes_database_pull.get_node_info(
                 nodes.loc[nodes['NodeName'] == nodeSelectShort].iloc[0]['Node_ID'], bit, dataSelect)
             if data.empty:
                 data = temp
             else:
                 data[dataSelect] -= temp[dataSelect]
-
+        
+        # Select short node
         nodeSelectLong = st.selectbox(
             "Which Node Short On " + point + "?",
             allNodes
         )
         dataTemp = pd.DataFrame()
+        
+        # Get the RT-DA information
         for bit in components['RT-DA']:
+            # Get bit of information
             temp = nodes_database_pull.get_node_info(
                 nodes.loc[nodes['NodeName'] == nodeSelectLong].iloc[0]['Node_ID'], bit, dataSelect)
+            
+            # Select information and subtract
             if dataTemp.empty:
                 dataTemp = temp
             else:
                 dataTemp[dataSelect] -= temp[dataSelect]
+        
+        # Add them together
         data[dataSelect] += dataTemp[dataSelect]
         nodeSelect = nodeSelectShort + " and " + nodeSelectLong
     else:
+        # Otherwise, put get information
         nodeSelect = "All"
         data = weather_temperature_pull.get_information(dataSelect)
 
@@ -240,24 +313,31 @@ def info_picker(nodeOptions, iems, nodeExclusive, allNodes, components, nodes, p
 
 
 
-
+# Filter the information
 def filter(data, nodeOptions, point, iems, nodeExclusive, allNodes, components, nodes):
-    # FILTER STUFF
+    # Get info to filter
     node, toShow, dataName = info_picker(nodeOptions[:-4]+[nodeOptions[-1]], iems, nodeExclusive, allNodes, components, nodes, "Filter " + point)
     st.write(toShow[dataName].dropna(axis=0, how='any').sort_values().reset_index(drop=True))
-
+    
+    # Select direction to filter
     direction = st.selectbox(
         "Which direction to filter on " + point + "?",
         ("Greater Than", "Less Than", "Equal To")
     )
 
-    # Choose the limit
+    # Select the limit
     limit = st.number_input("What is the limit on " + point + "?")
+    
+    # Make filter and get dates
     filterDates = filter_finder.filter_helper(dataName, direction, limit, node)
+    
+    # If you get dates, merge them
     if len(filterDates) != 0:
         data = pd.merge(filterDates, data, left_on=['PriceDate', 'Hour'], right_on=['PriceDate', 'Hour'])
     else:
         st.write("Failure")
+        
+    # Sort and drop values
     data.sort_values(by=['PriceDate', 'Hour'], inplace=True)
     data.dropna(axis=0, how='any', inplace=True)
     return data
